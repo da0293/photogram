0a67abab2772c4af21e12b299747bdc04edae160
da0293 <dy7133@naver.com>
2024-03-28 23:01:50 +0900

    [chore] 시큐리티태그 라이브러리 설정

--- photogram/src/main/java/com/photogram/web/UserController.java
+++ photogram/src/main/java/com/photogram/web/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -21,14 +22,15 @@ public class UserController {
 	@GetMapping("/user/{id}/update")
 	public String update(@PathVariable int id, 
 			// 해당 세션 접근 
-			@AuthenticationPrincipal PrincipalDetails principalDetails ) {
+			@AuthenticationPrincipal PrincipalDetails principalDetails, 
+			Model model) {
 		// 추천
 		System.out.println("세션정보: " + principalDetails.getUser());
 		// 극혐 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication(); 
 		PrincipalDetails mPrincipalDetails = (PrincipalDetails)auth.getPrincipal();
 		System.out.println("직접 찾은 세션정보: "+mPrincipalDetails.getUser());
-		
+		model.addAttribute("principal", principalDetails.getUser());
 		return "user/update";
 	}
 }
--- photogram/src/main/webapp/WEB-INF/views/layout/header.jsp
+++ photogram/src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -1,6 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%-- <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%> --%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%> 
+
+<!-- 인증된 정보(세션)에 접근하는 방법 -->
+<sec:authorize access="isAuthenticated()">
+    <sec:authentication property="principal" var="principal"/>
+</sec:authorize>
+
 
 <!DOCTYPE html>
 <html lang="en">
